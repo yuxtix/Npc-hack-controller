686f705e684861ea2655f1d883c783b6309dcf9c
Doal <130514197+yuxtix@users.noreply.github.com>
2025-07-28 18:04:03 -0500

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,120 @@
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+WindUI:GetTransparency(true)
+
+local Window = WindUI:CreateWindow({
+    Title = "Controller avatar",
+    Icon = "door-open",
+    Author = "Xime and Yuxtix",
+    Folder = "CloudHub",
+    Size = UDim2.fromOffset(580, 460),
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    Background = "", -- rbxassetid only
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+    User = {
+        Enabled = true,
+        Anonymous = false,
+        Callback = function()
+            print("clicked")
+        end,
+    },
+    
+})
+
+
+local Tab = Window:Tab({
+    Title = "Main",
+    Icon = "bird",
+    Locked = false,
+})
+
+
+
+
+local Button = Tab:Button({
+    Title = "Scan",
+    Desc = "Scan all the Characters",
+    Locked = false,
+    Callback = function()
+        for _, obj in ipairs(workspace:GetDescendants()) do
+		if obj:IsA("Model") and obj:FindFirstChildOfClass("Humanoid") then
+
+                    print("Character encontrado: " .. obj.Name)
+
+                    local XD = Tab:Button({
+                     Title = obj.Name,
+                    Desc = "Select",
+                    Locked = false,
+                    Callback = function()
+
+                    local npc = obj:clone()
+                    npc.Parent = workspace
+                    game.Players.LocalPlayer.Character = npc
+                    workspace.Camera.CameraSubject = npc
+                    npc.Humanoid.WalkSpeed = 16
+
+                     end
+                    })
+		end
+	end
+    end
+
+})
+
+
+
+
+local Tab = Window:Tab({
+    Title = "Normal",
+    Icon = "bird",
+    Locked = false,
+})
+
+
+
+
+local Button = Tab:Button({
+    Title = "Scan",
+    Desc = "Scan all the Characters",
+    Locked = false,
+    Callback = function()
+        for _, obj in ipairs(workspace:GetDescendants()) do
+		if obj:IsA("Model") and obj:FindFirstChildOfClass("Humanoid") then
+
+                    print("Character encontrado: " .. obj.Name)
+
+                    local XD = Tab:Button({
+                     Title = obj.Name,
+                    Desc = "Select",
+                    Locked = false,
+                    Callback = function()
+
+                    obj.Parent = workspace
+                    game.Players.LocalPlayer.Character = obj
+                    workspace.Camera.CameraSubject = obj
+                    obj.Humanoid.WalkSpeed = 16
+
+                     end
+                    })
+		end
+	end
+    end
+
+})
+
+
+
+
+
+
+function GetAllCharacterNames()
+	for _, obj in ipairs(workspace:GetDescendants()) do
+		if obj:IsA("Model") and obj:FindFirstChildOfClass("Humanoid") then
+			print("Character encontrado: " .. obj.Name)
+		end
+	end
+end
